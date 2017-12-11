040b7c0aaffd7dfc424e3bac62d8d4fa510f83cb
ethanbence <33294714+ethanbence@users.noreply.github.com>
2017-12-10 19:11:53 -0500

    Add files via upload

---
+++ hello_aliens.R
@@ -0,0 +1,7 @@
+"Hello extraterrestrials,
+
+Welcome here, it's pretty cool right? Where are you guys from?
+
+I think you'll find out that we are pretty welcoming here, and that we have a lot of cool things to offer. You have to check out as much as you can while you're staying here.
+
+Anyway, enjoy your time here and don't kill us."
